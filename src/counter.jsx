e2f0737f65652d62f85fa3860e59ddc82ebd2109
Faith Muthoni <faith@Faiths-MacBook-Pro.local>
2022-11-17 15:47:19 +0300

    add formcount method

--- src/counter.jsx
+++ src/counter.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+    state ={
+        count:0
+    };
     render() { 
         return (
             <div>
-                <h1>hey</h1>
+                <p>{this.state.count}</p>
+                <button>Add</button>
             </div>
         );
+    }
+
+    // method1
+    formatCount(){
+        return this.state.count == 0? 'Zero': this.state.count;
     }
 }
  
