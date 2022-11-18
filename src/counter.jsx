a97d0a4d0b589e4676ae41ea19d31a0967fdac90
Faith Muthoni <faith@Faiths-MacBook-Pro.local>
2022-11-18 12:03:31 +0300

    dynamic class render

--- src/counter.jsx
+++ src/counter.jsx
@@ -3,20 +3,27 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state ={
         count:0
+
+
     };
     render() { 
+        let classes = 'btn m-2 btn-';
+        classes += this.state.count === 0 ? 'danger': 'primary';
+        
         return (
             <div>
-                <p>{this.state.count}</p>
-                <button>Add</button>
+                <p className= { classes } >{this.formatCount()}</p>
+                <button  className = 'btn btn-secondary m-2'>Add</button>
             </div>
         );
     }
 
     // method1
     formatCount(){
-        return this.state.count == 0? 'Zero': this.state.count;
+        return this.state.count == 0?  'Zero' : this.state.count;
     }
+    
+  
 }
  
 export default Counter;
